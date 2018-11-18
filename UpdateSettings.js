dad1090070610c247f56a1bc5f36bc5456aa5822
Ray <xinzhengoh@csus.edu>
2018-11-17 16:41:20 -0800

    This is the final project for CSC 165 - Ray Goh

---
+++ UpdateSettings.js
@@ -0,0 +1,16 @@
+var JavaPackages = new JavaImporter(
+
+ );
+ 
+with (JavaPackages)
+{
+function updateStats(dolphin)
+{
+dolphin.setLocalScale(2.0,2.0,2.0);
+}
+
+function updateSpeed(n)
+{
+n = 2;
+}
+}
